5f0b452d26c021c87675667d4a755ddd1a9f04b9
Salahe-eddine BOUHDJEUR <salahe-eddine.bouhdjeur@horizontrading.io>
2025-06-04 12:56:26 +0200

    🔧 feat(install): Enhance install.sh with user prompts for system updates, dependency installation, and SSH configuration

--- install.sh
+++ install.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
 set -e
 
-echo "[+] Updating system..."
-apt update && apt upgrade -y
+ask_step() {
+    read -p "⏩ [*] Would you want to execute this step : $1 ? [y/n] " response
 
-echo "[+] Installing dependencies..."
-apt install -y net-tools
+    if [[ "$response" =~ ^[Yy]$ ]]; then
+        return 0
+    else
+        echo "⏩ [*] Skipping step : $1 "
+        return 1
+    fi
+}
 
-# echo "[+] Configuring SSH..."
-# bash scripts/setup-ssh.sh
+if ask_step "Updating system"; then
+    echo "[+] Updating system..."
+    apt update && apt upgrade -y
+fi
+
+if ask_step "Installing dependencies"; then
+    echo "[+] Installing dependencies..."
+    apt install -y net-tools
+    apt install -y zip
+fi
+
+if ask_step "Configuring SSH"; then
+    echo "[+] Configuring SSH..."
+    bash scripts/setup-ssh.sh
+fi
