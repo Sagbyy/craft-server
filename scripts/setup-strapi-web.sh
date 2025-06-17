960d0a1288ff6f7bc00ebf8eb0d3675c0259296c
Sagbyy <sbouhdjeur.gagny@gmail.com>
2025-06-17 22:25:58 +0200

    ✨ feat(env): Add example environment configuration and local environment file for Strapi setup; update .gitignore to exclude .env files

--- scripts/setup-strapi-web.sh
+++ scripts/setup-strapi-web.sh
@@ -1,5 +1,11 @@
+#!/bin/bash
+
+# 1. Install Caddy
 sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https curl
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
 sudo apt update
 sudo apt install caddy
+
+# 2. Install Node.js
+sudo apt install -y nodejs npm
