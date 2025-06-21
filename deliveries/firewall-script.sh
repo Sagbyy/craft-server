7f0b89666c411b913d9592f05a18ef797a004bd1
Sagbyy <sbouhdjeur.gagny@gmail.com>
2025-06-22 00:55:33 +0200

    ✨  feat(firewall): Replace iptables setup with a call to external firewall script for improved management

---
+++ deliveries/firewall-script.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Reset all rules
+sudo iptables -F # Flush all rules from filter table
+sudo iptables -t nat -F # Flush all rules from nat table
+sudo iptables -t mangle -F # Flush all rules from mangle table
+sudo iptables -X # Delete all custom chains
+
+# Set default policies to drop
+sudo iptables -P INPUT DROP # Drop all incoming traffic
+sudo iptables -P FORWARD DROP # Drop all forwarded traffic
+sudo iptables -P OUTPUT DROP # Drop all outgoing traffic
+
+# Loopback
+iptables -I INPUT -i lo -j ACCEPT # Accept all loopback traffic
+# Loopback is used for communication between the server and itself
+iptables -I OUTPUT -o lo -j ACCEPT # Accept all loopback traffic
+
+# Allow established and related connections
+# ESTABLISHED: The connection is established
+# RELATED: The connection is related to an existing connection
+iptables -I INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -I OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+# SSH
+iptables -I INPUT -p tcp --dport 22 -j ACCEPT # Accept all incoming SSH traffic
+iptables -I OUTPUT -p tcp --dport 22 -j ACCEPT # Accept all outgoing SSH traffic
+
+# Website (HTTP/HTTPS)
+# It used for the strapi web application
+iptables -I INPUT -p tcp --dport 80 -j ACCEPT # Accept all incoming HTTP traffic
+iptables -I INPUT -p tcp --dport 443 -j ACCEPT # Accept all incoming HTTPS traffic
+iptables -I OUTPUT -p tcp --dport 80 -j ACCEPT # Accept all outgoing HTTP traffic
+iptables -I OUTPUT -p tcp --dport 443 -j ACCEPT # Accept all outgoing HTTPS traffic
+
+# ICMP
+# It used for the ping command
+iptables -I INPUT -p icmp --icmp-type echo-request -j ACCEPT # Accept all incoming ping requests
+iptables -I OUTPUT -p icmp --icmp-type echo-reply -j ACCEPT # Accept all outgoing ping replies
+
+# PostgreSQL
+# It used for the PostgreSQL database
+iptables -I INPUT -p tcp --dport 5432 -j ACCEPT # Accept all incoming PostgreSQL traffic
+iptables -I OUTPUT -p tcp --dport 5432 -j ACCEPT # Accept all outgoing PostgreSQL traffic
+
+# SFTP
+# It used for the SFTP server
+iptables -I INPUT -p tcp --dport 2049 -j ACCEPT # Accept all incoming SFTP traffic
+iptables -I OUTPUT -p tcp --dport 2049 -j ACCEPT # Accept all outgoing SFTP traffic
+
+# Save rules
+sudo iptables-save > /etc/iptables/rules.v4
+
+echo "✅ Firewalls configured"
--- scripts/setup-firewalls.sh
+++ scripts/setup-firewalls.sh
@@ -1,37 +1,3 @@
 #!/bin/bash
 
-# Reset all rules
-sudo iptables -F
-sudo iptables -t nat -F
-sudo iptables -t mangle -F
-sudo iptables -X
-
-# Set default policies to drop
-sudo iptables -P INPUT DROP
-sudo iptables -P FORWARD DROP
-sudo iptables -P OUTPUT DROP
-
-# Loopback
-iptables -I INPUT -i lo -j ACCEPT
-iptables -I OUTPUT -o lo -j ACCEPT
-
-# Allow established and related connections
-iptables -I INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-iptables -I OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-
-# SSH
-iptables -I INPUT -p tcp --dport 22 -j ACCEPT
-iptables -I OUTPUT -p tcp --dport 22 -j ACCEPT
-
-# Website (HTTP/HTTPS)
-iptables -I INPUT -p tcp --dport 80 -j ACCEPT
-iptables -I INPUT -p tcp --dport 443 -j ACCEPT
-iptables -I OUTPUT -p tcp --dport 80 -j ACCEPT
-iptables -I OUTPUT -p tcp --dport 443 -j ACCEPT
-
-# ICMP (ping)
-iptables -I INPUT -p icmp --icmp-type echo-request -j ACCEPT
-iptables -I OUTPUT -p icmp --icmp-type echo-reply -j ACCEPT
-
-# Save rules
-sudo iptables-save > /etc/iptables/rules.v4 
+sudo ../deliveries/firewall-script.sh
